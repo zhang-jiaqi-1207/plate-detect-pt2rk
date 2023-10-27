bbecbbde5f93be999aa7a0f2f25f0c4ea1027a4d
Zhang Jiaqi <zhangjiaqi@qq.com>
2023-10-27 17:08:57 +0800

    Fourth commit!
    (1) Set the `QUANTIZE_ON` to `True`, which means do the quantization from `float16` to `asymmetric-int8`.
    (2) Use the `accuracy_analysis()` to dump the tensors and relative error of each layer.
    (3) Use the sample picture in the realistic deloyed place to act as calibration datasets.

--- simulation.py
+++ simulation.py
@@ -9,15 +9,15 @@ from utils.post_process import detect_Recognition_plate, detect_Recognition_plat
 
 
 # RKNN模型参数设定
-ONNX_MODEL = './weights/plate_detect.onnx'      # onnx文件路径，rknn.load_onnx()调用需要
-RKNN_MODEL = 'plate_detect.rknn'                # rknn导出路径位置
-IMG_PATH = './imgs/single_blue.jpg'             # 选择测试的图片
-QUANTIZE_ON = False                             # 是否进行量化。
-DATASET = './dataset.txt'                       # 在选择量化的前提下，进行量化的图片
+ONNX_MODEL = './weights/plate_detect.onnx'                # onnx文件路径，rknn.load_onnx()调用需要
+RKNN_MODEL = './weights/plate_detect.rknn'                # rknn导出路径位置
+IMG_PATH = '/mnt/c/Users/jxb/MyFiles/车牌/地库/苏F188W6.jpg'             # 选择测试的图片
+QUANTIZE_ON = True                            # 是否进行量化。
+DATASET = './dataset.txt'                       # 在选择量化的前提下，进行量化校准的图片
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-detect_model = load_model("./weights/plate_detect.pt", device)  #初始化检测模型
+# detect_model = load_model("./weights/plate_detect.pt", device)  #初始化检测模型
 plate_rec_model = init_model(device, "./weights/plate_rec_color.pth", is_color = True)      #初始化识别模型
 onnx_path = "./weights/plate_detect.onnx"
 
@@ -30,10 +30,13 @@ if __name__ == '__main__':
     # pre-process config
     print('--> Config model')
     rknn.config(
-        mean_values = [[75.17166682341139, 76.80874605712376, 78.56289270151774]],
-        std_values = [[40.4750835298387, 40.537190424613286, 40.09948233997197]],
+        mean_values = [[114.57069762499403, 112.06498372297212, 112.0936524574251]],
+        std_values = [[56.552368489533805, 59.74804638037939, 59.92431607351682]],
+        quantized_dtype = "asymmetric_quantized-8",
+        quantized_algorithm = 'normal',
+        quantized_method = 'channel',
         optimization_level = 2,
-        target_platform="rk3588")
+        target_platform = "rk3588")
     print('done')
 
     # Load ONNX model
@@ -49,6 +52,14 @@ if __name__ == '__main__':
     ret = rknn.build(do_quantization=QUANTIZE_ON, dataset=DATASET)
     if ret != 0:
         print('Build model failed!')
+        exit(ret)
+    print('done')
+
+    # Accuracy analysis
+    print('--> Accuracy analysis')
+    Ret = rknn.accuracy_analysis(inputs=[IMG_PATH])
+    if ret != 0:
+        print('Accuracy analysis failed!')
         exit(ret)
     print('done')
 
@@ -75,12 +86,12 @@ if __name__ == '__main__':
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
     # (2) 通过pt, onnx, rknn文件检测车牌，并识别别车牌
-    dict_list_torch = detect_Recognition_plate(detect_model,
-                                               img,
-                                               device,
-                                               plate_rec_model,
-                                               img_size = 640,
-                                               is_color = True)
+    # dict_list_torch = detect_Recognition_plate(detect_model,
+    #                                            img,
+    #                                            device,
+    #                                            plate_rec_model,
+    #                                            img_size = 640,
+    #                                            is_color = True)
     dict_list_onnx = detect_Recognition_plate_onnx(onnx_path,
                                                 img,
                                                 device,
@@ -95,11 +106,11 @@ if __name__ == '__main__':
                                                 is_color = True)
     
     # (3) 将onnx模型文件输出结果 与 rknn模型文件推理结果 输出比对
-    information_template = "- "*60 + "\n" + "{}" + "\n" + "- "*60
+    # information_template = "- "*60 + "\n" + "{}" + "\n" + "- "*60
     # print("-> "*10 + " ONNX OUTPUT " + "<- "*10)
     # print(information_template.format(dict_list_onnx))
-    print("-> "*10 + " RKNN OUTPUT " + "<- "*10)
-    print(information_template.format(dict_list_rknn))
+    # print("-> "*10 + " RKNN OUTPUT " + "<- "*10)
+    # print(information_template.format(dict_list_rknn))
  
 
     # (4) 将结果保存为图片
@@ -108,10 +119,10 @@ if __name__ == '__main__':
     # ori_img_torch = draw_result(img_torch, dict_list_torch)
     # cv2.imwrite("./results/result_torch.jpg", ori_img_torch)
 
-    # img_onnx = copy.deepcopy(img)
-    # print("onnx output:    ", end="")
-    # ori_img_onnx = draw_result(img_onnx, dict_list_onnx)
-    # cv2.imwrite("./results/result_onnx.jpg", ori_img_onnx) 
+    img_onnx = copy.deepcopy(img)
+    print("onnx output:    ", end="")
+    ori_img_onnx = draw_result(img_onnx, dict_list_onnx)
+    cv2.imwrite("./results/result_onnx.jpg", ori_img_onnx) 
 
     img_rknn = copy.deepcopy(img)
     print("rknn output:    ", end="")
